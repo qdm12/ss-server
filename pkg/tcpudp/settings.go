da5711cda74a1f260c2fac36d3944db0646a1edf
Quentin McGaw <quentin.mcgaw@gmail.com>
2023-05-31 12:43:42 +0000

    change(pkg): settings method `MergeWith` returns merged result

--- pkg/tcpudp/settings.go
+++ pkg/tcpudp/settings.go
@@ -80,15 +80,16 @@ func (s Settings) toUDP() (settings udp.Settings) {
 	return settings
 }
 
-// MergeWith sets unset fields of the receiving settings
-// with field values from the other settings.
-func (s *Settings) MergeWith(other Settings) {
-	s.Address = gosettings.MergeWithString(s.Address, other.Address)
-	s.LogAddresses = gosettings.MergeWithPointer(s.LogAddresses, other.LogAddresses)
-	s.CipherName = gosettings.MergeWithString(s.CipherName, other.CipherName)
-	s.Password = gosettings.MergeWithPointer(s.Password, other.Password)
-	s.TCP.MergeWith(other.TCP)
-	s.UDP.MergeWith(other.UDP)
+// MergeWith returns the merge result of the receiver settings with
+// any unset fields set to the field of the other settings argument.
+func (s *Settings) MergeWith(other Settings) (result Settings) {
+	result.Address = gosettings.MergeWithString(s.Address, other.Address)
+	result.LogAddresses = gosettings.MergeWithPointer(s.LogAddresses, other.LogAddresses)
+	result.CipherName = gosettings.MergeWithString(s.CipherName, other.CipherName)
+	result.Password = gosettings.MergeWithPointer(s.Password, other.Password)
+	result.TCP = s.TCP.MergeWith(other.TCP)
+	result.UDP = s.UDP.MergeWith(other.UDP)
+	return result
 }
 
 // OverrideWith sets any field of the receiving settings
